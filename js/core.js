29aadea457af4f9c2f2292a3226e80d057e509ab
KemelbekovDaniyar <kemelbekovdaniyar0@gmail.com>
2023-12-16 20:31:24 +0600

    first commit

---
+++ js/core.js
@@ -0,0 +1,9 @@
+document.getElementById("openPdfButton").addEventListener("click", function () {
+  var pdfUrl = "./img/pdf/Физика 10-класс(okuma.kg)_Физика, Техника61.pdf"; 
+  window.open(pdfUrl, "_blank");
+});
+
+document.getElementById("openPdfButtonA").addEventListener("click", function () {
+  var pdfUrl = "./img/pdf/Алгебра жана анализдин башталышы 10-11 класс.pdf"; 
+  window.open(pdfUrl, "_blank");
+});
