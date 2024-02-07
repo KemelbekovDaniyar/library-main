6888392c8bfa026012a7895baa9395599bc9e2c9
KemelbekovDaniyar <kemelbekovdaniyar0@gmail.com>
2024-02-07 09:13:49 +0600

    ad new book

--- js/core.js
+++ js/core.js
@@ -1,9 +1,16 @@
-document.getElementById("openPdfButton").addEventListener("click", function () {
-  var pdfUrl = "./img/pdf/Физика 10-класс(okuma.kg)_Физика, Техника61.pdf"; 
-  window.open(pdfUrl, "_blank");
-});
+document.getElementById('openPdfButton').addEventListener('click', function () {
+	var pdfUrl = './img/pdf/Физика 10-класс(okuma.kg)_Физика, Техника61.pdf'
+	window.open(pdfUrl, '_blank')
+})
 
-document.getElementById("openPdfButtonA").addEventListener("click", function () {
-  var pdfUrl = "./img/pdf/Алгебра жана анализдин башталышы 10-11 класс.pdf"; 
-  window.open(pdfUrl, "_blank");
-});
+document
+	.getElementById('openPdfButtonA')
+	.addEventListener('click', function () {
+		var pdfUrl = './img/pdf/Алгебра жана анализдин башталышы 10-11 класс.pdf'
+		window.open(pdfUrl, '_blank')
+	})
+
+document.getElementById('9Math').addEventListener('click', function () {
+	var pdfUrl = './img/pdf/Algebra_9_Makarychev_2017.pdf'
+	window.open(pdfUrl, '_blank')
+})
